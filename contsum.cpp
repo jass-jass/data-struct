7bc1ae72b96c36225e3bf3b9e48db363ac564d8b
Priya Jasmin <priyar.ec19@bmsce.ac.in>
2021-06-02 14:32:13 +0530

    Initial commit - got errors
    finds the max sum obtained from adding continuous elements

---
+++ contsum.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void input_array(int n, int arr[n])
+{
+	for(int i=0; i<n; i++)
+	{
+		cin >> arr[n];
+	}
+}
+
+int largest(vector<int> sum)
+{
+	sort(sum.begin(), sum.end());
+	int last = sum.size();
+	return sum[--last];
+}
+
+int continuous_sum(int n, int arr[n])
+{
+	int sum;
+	vector<int> sum_vect;
+	for(int i=0; i<n; i++)
+	{
+		sum = arr[i];
+		sum_vect.push_back(sum);
+		for(int j=(i+1); j<n; j++)
+		{
+			sum += arr[j];
+			sum_vect.push_back(sum);
+		}
+	}
+	return largest(sum_vect);
+}
+
+int main(void)
+{
+	int n;
+	cin >> n;
+	int arr[n];
+	input_array(n, arr);
+	int max_sum = continuous_sum(n, arr);
+	cout << max_sum;
+	return 0;
+}
